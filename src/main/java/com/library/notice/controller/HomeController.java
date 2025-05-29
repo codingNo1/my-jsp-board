59e420199a6bc7d8b7c5f9b1f4faaa5012744f20
Changhun Jung <hun3695@gamil.com>
2025-05-29 16:58:35 +0900

    library

---
+++ src/main/java/com/library/notice/controller/HomeController.java
@@ -0,0 +1,21 @@
+package com.library.notice.controller;
+
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController {
+		
+	@GetMapping("/home")
+	public String index() {
+		
+		return "index";
+	}
+}
+
+
+
+
+
+
