4339391498562ff727a693b86bb09baea4ad7602
Changhun Jung <hun3695@gmail.com>
2025-05-30 17:29:33 +0900

    re

--- src/main/java/com/library/notice/controller1/HomeController.java
+++ src/main/java/com/library/notice/controller1/HomeController.java
@@ -1,12 +1,14 @@
-package com.library.notice;
+package com.library.notice.controller1;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class HomeController {
-  @GetMapping({"/home"})
-  public String index() {
-    return "index";
-  }
+        
+    @GetMapping("/home")
+    public String index() {
+        
+        return "index";
+    }
 }
