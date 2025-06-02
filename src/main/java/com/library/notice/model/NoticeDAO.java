ce643f5f688a64f27166de45b5f58f605383f995
Changhun Jung <hun3695@gmail.com>
2025-06-02 15:33:00 +0900

    aa

--- src/main/java/com/library/notice/model/NoticeDAO.java
+++ src/main/java/com/library/notice/model/NoticeDAO.java
@@ -1,16 +1,11 @@
 package com.library.notice.model;
 
-import com.library.notice.model.Notice;
 import java.util.List;
 
 public interface NoticeDAO {
-  List<Notice> findAll();
-  
-  Notice findByNum(Long paramLong);
-  
-  void save(Notice paramNotice);
-  
-  void update(Notice paramNotice);
-  
-  void delete(Long paramLong);
+	List<Notice> findAll();
+	Notice findByNum(Long num);
+	void save(Notice notice);
+	void update(Notice notice);
+    void delete(Long num);     
 }
--- src/main/java/com/library/notice/service/NoticeService.java
+++ src/main/java/com/library/notice/service/NoticeService.java
@@ -1,17 +1,15 @@
 package com.library.notice.service;
 
-
-import com.library.notice.model.Notice;
 import java.util.List;
 
+import com.library.notice.model.Notice;
+
 public interface NoticeService {
-  List<Notice> findAll();
-  
-  Notice findByNum(Long paramLong);
-  
-  void write(Notice paramNotice);
-  
-  void update(Notice paramNotice);
-  
-  void delete(Long paramLong);
+
+	List<Notice> findAll();
+	Notice findByNum(Long num);
+	void write(Notice notice);
+	void update(Notice notice);
+	void delete(Long num);
+
 }
